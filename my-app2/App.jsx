17a081e9d09f6a1e7fdff4b4bef9c251af3c4a5d
iangmitchell <ian.g.mitchell@proton.me>
2025-05-12 12:58:15 +0100

    added .jsx for vite

---
+++ my-app2/App.jsx
@@ -0,0 +1,17 @@
+import "bulma/css/bulma.css";
+import Header from "./Header";
+import Name from "./Name";
+//import ArrayIntAsList from './ArrayIntAsList';
+function App() {
+  return (
+    <>
+      <Header />
+      {/* w15 ex2 */}
+      <Name />
+      {/* w15 ex3 */}
+      {/*<ArrayIntAsList/>*/}
+    </>
+  );
+}
+
+export default App;
---
+++ my-app2/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <>
+      <div class="block">
+        <section class="hero is-danger">
+          <div class="hero-body">
+            <h1 class="title"> Header </h1>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Header;
